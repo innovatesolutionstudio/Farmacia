b323c432ec02c375bca39340afb05b7b8d320bf1
innovatesolutionstudio <innovatesolutionstudio@gmail.com>
2024-11-04 22:41:57 -0400

    compras acabado

--- views/graficos/js/datos.js
+++ views/graficos/js/datos.js
@@ -1,22 +1,30 @@
-   
 // Hacer una solicitud al servidor para obtener los datos
-fetch('/datos')
-  .then(response => response.json())
-  .then(data => {
+fetch("/datos")
+  .then((response) => response.json())
+  .then((data) => {
     // Actualizar los divs con los datos recibidos
-    document.getElementById('ventasMes').innerText = ' Bs. '+  data.ventasMes;
-    document.getElementById('ventasSemana').innerText = data.ventasSemana + ' Bs.';
-    document.getElementById('clientesRegistradosMes').innerText =  data.clientesRegistradosMes;
-    document.getElementById('medicamentosapuntovencer').innerText =  data.medicamentosapuntovencer;
-    document.getElementById('obtenerVentasMest').innerText =  data.obtenerVentasMest;
-    document.getElementById('obtenerProductoMasVendido').innerText =  data.obtenerProductoMasVendido || 'No hay ventas este año'; // Manejar el caso null
-    document.getElementById('obtenerTotalProductosVendidosMesSucursal').innerText =  data.obtenerTotalProductosVendidosMesSucursal|| 'No hay ventas este mes';
-    document.getElementById('obtenerCantidadTotalInventario').innerText =  data.obtenerCantidadTotalInventario || 'Inventario vacio'; // Manejar el caso null
-    document.getElementById('obtenerProveedorMasComprado').innerText =  data.obtenerProveedorMasComprado || 'no hay proveedores'; // Manejar el caso null
-    document.getElementById('obtenerTotalComprasC').innerText =  data.obtenerTotalComprasC + ' Bs.'|| 'No hay compras';
-    
-
+    document.getElementById("ventasMes").innerText = " Bs. " + data.ventasMes;
+    document.getElementById("ventasSemana").innerText =
+      data.ventasSemana + " ventas";
+    document.getElementById("clientesRegistradosMes").innerText =
+      data.clientesRegistradosMes;
+    document.getElementById("medicamentosapuntovencer").innerText =
+      data.medicamentosapuntovencer;
+    document.getElementById("obtenerVentasMest").innerText =
+      data.obtenerVentasMest;
+    document.getElementById("obtenerProductoMasVendido").innerText =
+      data.obtenerProductoMasVendido || "No hay ventas este año"; // Manejar el caso null
+    document.getElementById(
+      "obtenerTotalProductosVendidosMesSucursal"
+    ).innerText =
+      data.obtenerTotalProductosVendidosMesSucursal || "No hay ventas este mes";
+    document.getElementById("obtenerCantidadTotalInventario").innerText =
+      data.obtenerCantidadTotalInventario || "Inventario vacio"; // Manejar el caso null
+    document.getElementById("obtenerProveedorMasComprado").innerText =
+      data.obtenerProveedorMasComprado || "no hay proveedores"; // Manejar el caso null
+    document.getElementById("obtenerTotalComprasC").innerText =
+      data.obtenerTotalComprasC + " Bs." || "No hay compras";
   })
-  .catch(error => {
-    console.error('Error fetching data:', error);
+  .catch((error) => {
+    console.error("Error fetching data:", error);
   });
